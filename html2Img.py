88a6dbbb637c4d085e7ab0486f571301a11a4a1f
13522955396@163.com <Git5241osc>
2017-01-12 11:57:40 +0800

    添加版本控制

--- html2Img.py
+++ html2Img.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 # 中文为方框的话需要安装字体
 # 在centos中执行：yum install bitmap-fonts bitmap-fonts-cjk
-# 在ubuntu中执行：sudo apt-get install
+# 在ubuntu中执行：sudo apt-get install xfonts-wqy
 # 如果要截取 html 文件需要使用 file:///D:/WebstormProjects/ZuiBlog/index.html 类似这样的方式
 # platform Windows-10-10.0.15002-SP0  Linux-3.10.104-1.el6.elrepo.i686-i686-with-centos-6.8-Final  Darwin
 
